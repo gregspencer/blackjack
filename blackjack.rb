c1a8cd75769dc39e039b65374593814f541039c1
Greg Spencer <spencega@gmail.com>
2014-07-01 15:17:50 -0500

    a bit more work

--- blackjack.rb
+++ blackjack.rb
@@ -12,30 +12,37 @@ player_hand << deck.pop
 computer_hand << deck.pop
 
 def calculate_total(cards)
-	arr = cards.map {|e| e[1].to_i}
-	total = 0
-	arr.each do |value|
-		if value == 0
-			total += 10
-		else
-			total += value
-		end
-	end
+  arr = cards.map {|e| e[1].to_i}
+  total = 0
+  arr.each do |value|
+    if value == 0
+      total += 10
+    else
+      total += value
+    end
+  end
 end
 
 puts "Here is your hand:"
 player_hand.each do |card|
-	puts "#{card.first} of #{card.last}"
-	end
-	puts ' '
-	puts "What do you want to do? 
-	(1) Stay
-	(2) Hit"
-	player_action = gets.chomp
-	if player_action == "2"
-		player_hand << deck.pop
-	elsif player_action == "1"
-		nil
+  puts "#{card.first} of #{card.last}"
+  end
+  puts ' '
+  puts "What do you want to do? 
+  (1) Stay
+  (2) Hit"
+  player_action = gets.chomp
+  if player_action == "2"
+    player_hand << deck.pop
+    puts "Here is your hand:"
+    player_hand.each do |card|
+    puts "#{card.first} of #{card.last}"
+    end
+  elsif player_action == "1"
+    puts "Here is your hand:"
+    player_hand.each do |card|
+    puts "#{card.first} of #{card.last}"
+  end
 end
 
-player_turn(player_hand)
+
